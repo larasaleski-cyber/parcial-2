cc1aa58054bb515b1697089d59ab26154f0143ec
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-17 09:34:02 -0300

    Create desafio 1.py

---
+++ desafio 1.py
@@ -0,0 +1,7 @@
+# Solicita dois números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Soma os dois números e exibe o resultado
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}")
