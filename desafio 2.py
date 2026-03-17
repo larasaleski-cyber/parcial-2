5b6897a0d7f4c95f9b3975947eea4906086316d3
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-17 09:40:44 -0300

    Create desafio 2.py

---
+++ desafio 2.py
@@ -0,0 +1,8 @@
+# Solicita um número ao usuário
+num = int(input("Digite um número: "))
+
+# Verifica se o número é par ou ímpar
+if num % 2 == 0:
+    print(f"O número {num} é par.")
+else:
+    print(f"O número {num} é ímpar.")
