64a6502ec973ee34eaa3a490b4bf00cda3f2b5a4
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-25 08:28:31 -0300

    Create desafio 3.py

---
+++ desafio 3.py
@@ -0,0 +1,9 @@
+# Programa para armazenar 5 nomes e imprimir todos
+
+# Cria uma lista com 5 nomes
+nomes = ["Roberta", "Sthefany", "Miguel", "Daniela", "Renata"]
+
+# Imprime cada nome da lista usando um loop
+print("Os nomes na lista são:")
+for nome in nomes:
+    print(nome)
