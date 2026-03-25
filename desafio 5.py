af8a845a2331cb3866c7e54de7c512261c3a63ec
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-25 08:43:44 -0300

    Create desafio 5.py

---
+++ desafio 5.py
@@ -0,0 +1,11 @@
+# Programa para calcular a área de um triângulo
+
+# Solicitar ao usuário a base e a altura do triângulo
+base = float(input(" valor da base do triângulo: "))
+altura = float(input("valor da altura do triângulo: "))
+
+# Calcula a área usando a fórmula: área = (base * altura) / 2
+area = (base * altura) / 2
+
+# mostrando resultado
+print("A área do triângulo é: {area}")
