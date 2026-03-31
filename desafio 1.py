869a8ecc828b95a095bdd7fabd508f399df9354f
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-31 09:27:05 -0300

    Update desafio 1.py

--- desafio 1.py
+++ desafio 1.py
@@ -2,6 +2,6 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-# Soma os dois números e exibe o resultado
+# Soma os dois números e indicando o resultado
 soma = num1 + num2
 print(f"A soma de {num1} e {num2} é {soma}")
