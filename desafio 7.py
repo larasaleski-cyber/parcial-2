d395c01f2e9b7802784591dfdc35de4365febb4d
larasaleski-cyber <lara.saleski@escola.pr.gov.br>
2026-03-31 09:24:30 -0300

    Create desafio 7.py

---
+++ desafio 7.py
@@ -0,0 +1,12 @@
+# determinar juros simples
+
+# valores
+C = float(input("Digite o capital (C): "))     # Capital investido
+I = float(input("Digite a taxa de juros (I) %: "))  # Taxa de juros em porcentagem
+T = float(input("Digite o tempo (T) em meses ou anos: "))  # Tempo do investimento
+
+# Fórmula de juros simples: J = (C * I * T) / 100
+J = (C * I * T) / 100
+
+# Mostra o resultado bonitinho
+print(f"O valor dos juros simples é: {J:.2f}")
